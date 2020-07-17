cb5fa5a5a0a32de578287f6e4fec84af9a738388
sunghs <sunghs9071@naver.com>
2020-07-18 00:00:27 +0900

    Initial commit

---
+++ src/main/java/sunghs/leetcode/solve/LeetcodeSolveApplication.java
@@ -0,0 +1,13 @@
+package sunghs.leetcode.solve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeSolveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeSolveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sunghs/leetcode/solve/LeetcodeSolveApplicationTests.java
@@ -0,0 +1,13 @@
+package sunghs.leetcode.solve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeSolveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
