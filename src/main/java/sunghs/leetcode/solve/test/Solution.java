8031767caeea83573a4245db78a1642cef35d669
sunghs <sunghs9071@naver.com>
2020-07-21 16:36:59 +0900

    1021_Remove_Outermost_Parentheses.md 추가

--- src/main/java/sunghs/leetcode/solve/test/Solution.java
+++ src/main/java/sunghs/leetcode/solve/test/Solution.java
@@ -1,5 +1,8 @@
 package sunghs.leetcode.solve.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Solution {
 
     public static void main(String[] args) {
@@ -14,5 +17,39 @@ public class Solution {
             result[csr++] = nums[i + n];
         }
         return result;
+    }
+
+    public String removeOuterParentheses(String S) {
+        StringBuilder sb = new StringBuilder(S);
+        int start = 0;
+        int end = 0;
+        ArrayList<Integer> remove = new ArrayList<>();
+        char[] parr = S.toCharArray();
+
+        for (int i = 0; i < parr.length; i++) {
+            if (parr[i] == '(') {
+                start++;
+            } else if (parr[i] == ')') {
+                end++;
+            }
+
+            if (start == 1 && end == 0) {
+                remove.add(i);
+            }
+            if (start == end && start > 0 && end > 0) {
+                remove.add(i);
+                start = 0;
+                end = 0;
+            }
+        }
+
+        Integer[] removeIndex = new Integer[remove.size()];
+        removeIndex = remove.toArray(removeIndex);
+        Arrays.sort(removeIndex);
+
+        for (int i = removeIndex.length - 1; i >= 0; i--) {
+            sb.deleteCharAt(removeIndex[i]);
+        }
+        return sb.toString();
     }
 }
